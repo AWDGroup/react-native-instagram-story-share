b4f80684d243789a59c0936a2b718c1c44a615f3
Ardavan Kalhori <ardavan.kalhori@outlook.com>
2018-09-28 01:42:41 -0700

    Added Android Support

--- android/src/main/java/com/jobeso/RNInstagramStoryShare/RNInstagramStoryShareModule.java
+++ android/src/main/java/com/jobeso/RNInstagramStoryShare/RNInstagramStoryShareModule.java
@@ -1,42 +1,20 @@
 package com.jobeso.RNInstagramStoryShare;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.pm.ResolveInfo;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.annotation.SuppressLint;
-import android.view.View;
-import android.view.WindowManager;
-import android.view.KeyCharacterMap;
-import android.util.DisplayMetrics;
-import android.view.KeyEvent;
-import android.view.ViewConfiguration;
-import android.view.Display;
-import android.content.Context;
-import android.util.Log;
-import android.content.Intent;
 import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.net.Uri;
-import android.support.annotation.Nullable;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
 
 import com.facebook.react.bridge.Promise;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.bridge.UiThreadUtil;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.WritableNativeMap;
-
-import okhttp3.MediaType;
 
 public class RNInstagramStoryShareModule extends ReactContextBaseJavaModule {
 
@@ -58,40 +36,73 @@ public class RNInstagramStoryShareModule extends ReactContextBaseJavaModule {
         return "RNInstagramStoryShare";
     }
 
+
     @ReactMethod
-    public void share(ReadableMap options, @Nullable Callback failureCallback, @Nullable Callback successCallback) {
-      try {
-        // Define image asset URI and attribution link URL
-          File imageFileToShare = new File("/storage/emulated/0/DCIM/Camera/IMG_20180530_212925.jpg");
-        Uri backgroundAssetUri = Uri.fromFile(imageFileToShare);//options.getString("backgroundImage"));
-        String attributionLinkUrl = options.getString("deeplinkUrl");
-
-
-        // Instantiate implicit intent with ADD_TO_STORY action,
-        // background asset, and attribution link
-        Intent intent = new Intent("com.instagram.share.ADD_TO_STORY");
-        intent.setDataAndType(backgroundAssetUri, MEDIA_TYPE_PNG);
-        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        intent.putExtra("content_url", attributionLinkUrl);
-
-        // Instantiate activity and verify it will resolve implicit intent
-        Activity activity = getCurrentActivity();
-        ResolveInfo should = activity.getPackageManager().resolveActivity(intent, 0);
-        if (should != null) {
-          activity.startActivityForResult(intent, 0);
-          successCallback.invoke("OK");
-        } else {
-          successCallback.invoke("OK, but not opened");
+    public void isInstagramInstalledOnDevice(Promise promise) {
+        try {
+            Intent intent = new Intent("com.instagram.share.ADD_TO_STORY");
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            Activity activity = getCurrentActivity();
+            if (activity.getPackageManager().resolveActivity(intent, 0) != null)
+            {
+                promise.resolve("OK");
+            }
+            else
+            {
+                promise.reject("ERROR", new Exception(NOT_INSTALLED));
+            }
+        } catch (Exception e) {
+            promise.reject("ERROR", e);
         }
 
+    }
 
 
+    private static boolean isPackageInstalled(Context c, String targetPackage) {
+        PackageManager pm = c.getPackageManager();
+        try {
+            PackageInfo info = pm.getPackageInfo(targetPackage, PackageManager.GET_META_DATA);
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+        return true;
+    }
 
-      } catch(ActivityNotFoundException ex) {
-          System.out.println("ERROR");
-          System.out.println(ex.getMessage());
-          failureCallback.invoke(ex);
-      }
+    @ReactMethod
+    public void share(ReadableMap options, Promise promise) {
+        try {
+            // Define image asset URI and attribution link URL
+            Uri backgroundAssetUri = Uri.parse(options.getString("backgroundImage"));
+            Uri stickerImageUri = Uri.parse(options.getString("stickerImage"));
+            String attributionLinkUrl = options.getString("deeplinkingUrl");
+
+
+            // Instantiate implicit intent with ADD_TO_STORY action,
+            // background asset, and attribution link
+            Intent intent = new Intent("com.instagram.share.ADD_TO_STORY");
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.setDataAndType(backgroundAssetUri, MEDIA_TYPE_JPEG);
+            intent.putExtra("interactive_asset_uri", stickerImageUri);
+            intent.putExtra("content_url", attributionLinkUrl);
+
+            // Instantiate activity and verify it will resolve implicit intent
+            Activity activity = getCurrentActivity();
+            activity.grantUriPermission(
+                    "com.instagram.android", stickerImageUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            if (activity.getPackageManager().resolveActivity(intent, 0) != null)
+            {
+                activity.startActivityForResult(intent, 0);
+                promise.resolve("OK");
+            }
+            else
+            {
+                promise.reject("ERROR", new Exception(NOT_INSTALLED));
+            }
+
+
+        } catch (ActivityNotFoundException ex) {
+            promise.reject("ERROR", ex);
+        }
     }
 
 }
